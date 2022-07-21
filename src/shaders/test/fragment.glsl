d34ecb5809e45913420229346d3b2f99f502b0a6
Mhd alali <hsinsa758@gmail.com>
2022-07-20 21:33:57 -0700

    distorted the positions

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -14,13 +14,11 @@ float random(vec2 st) {
     return fract(sin(dot(st.xy, vec2(12.9898, 78.232))) * 43758.5432123);
 }
 
-
 vec2 rotate(vec2 uv, float rotation, vec2 mid) {
     return vec2(cos(rotation) * (uv.x - mid.x) + sin(rotation) * (uv.y - mid.y) + mid.x, cos(rotation) * (uv.y - mid.y) - sin(rotation) * (uv.x - mid.x) + mid.y);
 }
 
 void main() {
-    float strength = 1.0;
-    strength += vElevation;
-    gl_FragColor = vec4(strength,strength,strength, 1.0);
+    float strength = vElevation + .5;
+    gl_FragColor = vec4(strength, strength, strength, 1.0);
 }
